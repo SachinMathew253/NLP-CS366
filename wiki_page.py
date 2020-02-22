1500d0153aa5c3dac823c9efab9fb84dde870cc3
Quenza <32495741+SachinMathew253@users.noreply.github.com>
2020-02-23 04:04:26 +0530

    Update wiki_page.py

--- wiki_page.py
+++ wiki_page.py
@@ -2,20 +2,22 @@ import wikipedia
 import string
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
+from collections import defaultdict
 
-
+end = "********************************************************************************************************************"
 f = open("number.txt" , 'w' , encoding = 'utf-8')
 f.write((wikipedia.page("number")).content)
 f.close()
 f = open("number.txt" , 'r' , encoding = 'utf-8')
 text = f.read()
 words = text.split()
-print("WikiPage on 'Number' is scraped. \nTotal No of Words: %d" %len(words))
+print("WikiPage on 'Number' is scraped. \nTotal No of Initial Words: %d" %len(words))
 print("\nRemoving Punctuations!")
 for c in string.punctuation:
     text = text.replace(c,"")
+text = text.replace('-',"")
 words = text.split()
-print("Completed! \nNo of Words: %d \n\nRemoving Numbers!" %len(words))
+print("Completed! (kindly note the '−' is a minus and not a hyphen '-')\nNo of Words: %d \n\nRemoving Numbers!" %len(words))
 for c in list(range(0,10)):
 	text = text.replace(str(c),"")
 print("Completed!")
@@ -43,3 +45,24 @@ f.close()
 print("Completed!  File saved as 'number_wikiEdit.txt'")
 print("No of Words: %d" %len(words))
 appendFile.close()
+print("\nFiles created during the execution. \n'number.txt' -> Original wikipage text\n'number_wiki.txt' -> edited with no Numbers and Punctuations\n'number_wikiEdit.txt' -> further edited with no stopwords")
+print("\n Below is the Text containing no stop words:\n")
+print(line)
+
+d = defaultdict(int)
+q = defaultdict(int)
+for w in text.split():
+  d[w] += 1
+
+for w in sorted(d, key=d.get, reverse=True):
+	q[w] = d[w]
+print(end)
+print("\nThe below is a dictionary containing the distinct words in file: 'number_wikiEdit.txt'\n")
+print(q)	
+uniq_word_value = list(q.values())
+uniq_word = list(q.keys())
+
+print(end)
+print("\nTotal Words: %d\nTotal Distinct Words: %d\n\nTop 10 most frequent words along with its count and Total percentage:\n" % (len(words) , len(uniq_word)))
+for i in range(10):
+	print(uniq_word[i] + ":\t\tCount-> " + str(uniq_word_value[i])+ "\tPercentage-> "+str(round((uniq_word_value[i]/len(words))*100,2)) +"%")
